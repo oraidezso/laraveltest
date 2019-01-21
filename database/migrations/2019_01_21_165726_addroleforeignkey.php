39eae651e0cd2314f5866b5ba1a4124d38290ba9
dezso <orai.dezso@gmail.com>
2019-01-21 18:44:24 +0100

    Add foreign keys to db

---
+++ database/migrations/2019_01_21_165726_addroleforeignkey.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Addroleforeignkey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('role_user', function($table)
+        {
+            $table->biginteger('role_id')->unsigned()->change();
+        });
+        Schema::table('role_user', function($table) {
+            
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('role_user', function($table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['role_id']);
+        });
+    }
+}
